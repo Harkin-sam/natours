406a4172ccff6aa33dd57f28d8db1407f7f3080e
Samuel Akinfolarin <Akinfolarin001@gmail.com>
2023-07-27 22:24:22 +0100

    stripe webhook

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 // const fs = require('fs');
-const path = require('path') // this is a built-in node module used to manipulate path
+const path = require('path'); // this is a built-in node module used to manipulate path
 
 // its kinda a general convention to have all EXPRESS configuration in app js
 // orders is really important in express
 
 const express = require('express');
-const morgan  = require('morgan');
+const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
@@ -14,32 +14,30 @@ const hpp = require('hpp');
 const compression = require('compression'); // to compress res json
 const cookieParser = require('cookie-parser');
 // in order to get access to the cookie in any incoming request, we use a package installed 'npm i cookie-parser'
-const cors = require('cors'); 
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
-const globalErrorHandler = require('./errorController')
-const tourRouter = require('./routes/tourRoutes.js')
+const globalErrorHandler = require('./errorController');
+const tourRouter = require('./routes/tourRoutes.js');
 
-const userRouter = require('./routes/userRoutes.js')
-const reviewRouter = require('./routes/reviewRoutes.js')
-const bookingRouter = require('./routes/bookingRoutes')
-const viewRouter = require('./routes/viewRoutes')
-
-
-
+const userRouter = require('./routes/userRoutes.js');
+const reviewRouter = require('./routes/reviewRoutes.js');
+const bookingRouter = require('./routes/bookingRoutes');
+const bookingController = require('./controllers/bookingController');
+const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
 //express is a function which upon calling will add a bunch of methods to app variable
 
-app.enable('trust proxy') // this is built-in express to enable proxy connection 
+app.enable('trust proxy'); // this is built-in express to enable proxy connection
 
 //SETTING UP our render template Engine (PUG)
 // npm i pug
 // pug is a simple white space sensitive syntax for writing html
-app.set('view engine', 'pug'); 
+app.set('view engine', 'pug');
 
 // Defining where this views is located in our file system, pug template are actually called views in express
-app.set('views', path.join(__dirname, 'views')) // this create a path joining the directory name /view
+app.set('views', path.join(__dirname, 'views')); // this create a path joining the directory name /view
 
 //1) GLOBAL MIDDLEWARES
 
@@ -50,76 +48,81 @@ app.use(cors());
 // eg lets say we have our our front and back end on diff domain
 // BE- api.natours.com, FE- natours.com
 // app.use(cors({
-//     origin: 'https://www.natours.com' // this will allow req form this site 
+//     origin: 'https://www.natours.com' // this will allow req form this site
 // }))
 // simple request ie post and get and complex request ( delete, patch )pre-flight phase, browser options and permission
 
 app.options('*', cors()); // allows all routes
-// app.options('/api/v1/tours/:id', cors()) 
+// app.options('/api/v1/tours/:id', cors())
 
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-// SETTING SECURITY HTTP HEADERS: we need to always put in the beginning of our code 
-// app.use(helmet()); 
+// SETTING SECURITY HTTP HEADERS: we need to always put in the beginning of our code
+// app.use(helmet());
 
 // in app.use we use a function itself not a function call name only
-app.use( helmet({ contentSecurityPolicy: false }) );
+app.use(helmet({ contentSecurityPolicy: false }));
 
+// DEVELOPMENT LOGGING
+if (process.env.NODE_ENV === 'development') {
+  // there is also third party middleware called morgan, which can be used for login and development cases, use npm i morgan
 
-
-// DEVELOPMENT LOGGING 
-if(process.env.NODE_ENV === 'development'){  
-// there is also third party middleware called morgan, which can be used for login and development cases, use npm i morgan
-
-app.use(morgan('dev')); // this provides much details about our req and res
+  app.use(morgan('dev')); // this provides much details about our req and res
 }
 
-
-//IMPLEMENTING RATE LIMITING: to prevent hacker using too many request to guess the credentials of a user 
+//IMPLEMENTING RATE LIMITING: to prevent hacker using too many request to guess the credentials of a user
 // limit request from same IP
 const limiter = rateLimit({
-    max: 100, // max no of requests
-    windowMs: 60 * 60 * 1000, // means per 1hr
-    message: 'Too many request from this IP, please try again in an hour' // this is the error message that will display
-})
+  max: 100, // max no of requests
+  windowMs: 60 * 60 * 1000, // means per 1hr
+  message: 'Too many request from this IP, please try again in an hour', // this is the error message that will display
+});
 
-app.use('/api', limiter) //using it as middleware
+app.use('/api', limiter); //using it as middleware
 
+// USING STRIPE WEBHOOKS
+app.post(
+  '/webhook-checkout',
+  express.raw({ type: 'application/json' }),
+  bookingController.webhookCheckout
+);
+
+// because we need the body in a raw form using express.raw for the function thats why we use app.post and not mounted in booking route i.e we need it as a stream not as json so that why it is placed before the BODY PARSER MIDDLEWARE below
 
 //BODY PARSER, reading data from the body into req.body
 
 // This is a middleware from express to access the request body,
-app.use(express.json({limit : '10kb'})); // the limit here will limit the size of the request body to 10kb to prevent infecting the body with malicious code
-app.use(express.urlencoded({extended: true, limit: '10kb'})) // an express built-in middleware to parse data from a url encoded form 
+app.use(express.json({ limit: '10kb' })); // the limit here will limit the size of the request body to 10kb to prevent infecting the body with malicious code
+app.use(express.urlencoded({ extended: true, limit: '10kb' })); // an express built-in middleware to parse data from a url encoded form
 
 // COOKIE PARSER
 app.use(cookieParser()); // parses the data from the cookie
 
+// DATA SANITIZATION against No SQL query injection
 
-// DATA SANITIZATION against No SQL query injection 
-
-app.use(mongoSanitize()); // this looks at the req.body and params and filter out all of the $ signs and sql query 
+app.use(mongoSanitize()); // this looks at the req.body and params and filter out all of the $ signs and sql query
 
 // DATA SANITIZATION against XSS
 
 app.use(xss()); // this will clean any use input from malicious html code
 
 // PREVENTING PARAMETER POLLUTION: i it helps to clean up the query string
-app.use(hpp({
+app.use(
+  hpp({
     whitelist: [
-        'duration',
-        'ratingsQuantity',
-        'ratingsAverage',
-        'maxGroupSize',
-        'difficulty',
-        'price'
-    ]
-})); // whitelist simply an array of properties we allow duplicates in the query string
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+); // whitelist simply an array of properties we allow duplicates in the query string
 
 // MIDDLEWARE a functions basically used to modify the incoming request, we use them to access and process the  'post'request from the client
 //express.json() here is the middleware, a middleware has access to the req, res and next function as 3rd args
-
 
 // we must define middleware before any of our route handler
 
@@ -127,7 +130,6 @@ app.use(hpp({
 //using the simple built in middleware
 // app.use(express.static(`${__dirname}/public`))
 // app.use(express.static(path.join(__dirname, 'public')));
-
 
 //CREATING OUR MIDDLEWARE
 // app.use((req, res, next) => {
@@ -137,25 +139,21 @@ app.use(hpp({
 //     next(); //never forget to use next in all of your middlewares or else the code will break
 // });
 
-app.use(compression()); // will compress all the test sent to the client 
-
-
+app.use(compression()); // will compress all the test sent to the client
 
 // Test Middleware
 app.use((req, res, next) => {
+  // we can also define custom property on the request object
+  req.requestTime = new Date().toISOString();
 
-    // we can also define custom property on the request object
-    req.requestTime = new Date().toISOString();
-
-    // console.log(req.cookies)
-    next();
-})
+  // console.log(req.cookies)
+  next();
+});
 
 //READFILE -> json
 // const tours = JSON.parse(
 //   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 // );
-
 
 // // ROUTE HANDLERS
 // const getAllTours = (req, res) => {
@@ -177,7 +175,6 @@ app.use((req, res, next) => {
 // //NB: you can have multiple parameters /api/v1/tours/:id/:x/:y
 
 // //NB: you can have optional parameters /api/v1/tours/:id/:x/:y? so y here is optional
-
 
 // //GET by ID
 // const getTour = (req, res) => {
@@ -263,9 +260,6 @@ app.use((req, res, next) => {
 //   });
 // };
 
-
-
-
 // const getAllUsers = (req, res) => {
 
 //     //error 500 means internal server error
@@ -315,9 +309,6 @@ app.use((req, res, next) => {
 
 // }
 
-
-
-
 // GET
 // app.get('/api/v1/tours', getAllTours);
 // app.get('/api/v1/tours/:id', getTour);
@@ -338,53 +329,49 @@ app.use((req, res, next) => {
 // const tourRouter = express.Router(); // here we created this new router and saved it in this variable, then we use it as middle ware ^^
 // const userRouter = express.Router();
 
-
 // tourRouter.route('/').get(getAllTours).post(createTour)
 
 // tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
- 
+
 //ROUTES
 app.use('/', viewRouter);
 
 // we use the mounted middleware located here after the  required variable has been declared
 app.use('/api/v1/tours', tourRouter);
-app.use ('/api/v1/users', userRouter);
+app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
 app.use('/api/v1/bookings', bookingRouter);
-
 
 // implementing 'Users' routes
 // userRouter.route('/').get(getAllUsers).post(createUser)
 
 // userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser)
 
-
 //HANDLING UNHANDLED ROUTES
-// since the middleware act according in the specific order as written in the code, this is position down in code stack so that any req that get here must have passed through all routes handlers, and if if it eventually get here it means it does not match or its not meant for the server so this middleware serves as the error message 
+// since the middleware act according in the specific order as written in the code, this is position down in code stack so that any req that get here must have passed through all routes handlers, and if if it eventually get here it means it does not match or its not meant for the server so this middleware serves as the error message
 
 app.all('*', (req, res, next) => {
-    // res.status(404).json({
-    //     status: 'fail',
-    //     message: `Cant find ${req.originalUrl} on this server`
-    // })
+  // res.status(404).json({
+  //     status: 'fail',
+  //     message: `Cant find ${req.originalUrl} on this server`
+  // })
 
-    // another syntax
-    // const err = new Error(`cant find ${req.originalUrl} on this server`)
-    // err.status = 'fail';
-    // err.statusCode = 404;
+  // another syntax
+  // const err = new Error(`cant find ${req.originalUrl} on this server`)
+  // err.status = 'fail';
+  // err.statusCode = 404;
 
-    // next(err); // once you pass in err args in the next function it signifies that the req is an err and instead of moving to the next middleware in the stack it automatically looks for the central error handling middleware and pass the arg to it to handle it
+  // next(err); // once you pass in err args in the next function it signifies that the req is an err and instead of moving to the next middleware in the stack it automatically looks for the central error handling middleware and pass the arg to it to handle it
 
-    // Better Error refactoring
-    next(new AppError(`cant find ${req.originalUrl} on this server`, 404 ))
-}); 
+  // Better Error refactoring
+  next(new AppError(`cant find ${req.originalUrl} on this server`, 404));
+});
 
-// .all() works for all the http methods, get,post, create and the rest 
+// .all() works for all the http methods, get,post, create and the rest
 // '*' stands for all routes
 
-
 // GLOBAL CENTRAL ERROR HANDLING MIDDLEWARE THAT WILL JUST CATCH ALL ERRORS AND HANDLE THEM ACCORDINGLY
-// if your  middleware has 4 args express will automatically know that it an error handling middleware and it will only call it when there is an error 
+// if your  middleware has 4 args express will automatically know that it an error handling middleware and it will only call it when there is an error
 
 app.use(globalErrorHandler);
 
@@ -394,39 +381,27 @@ app.use(globalErrorHandler);
 //   console.log(`App running on port ${port}...`);
 // });
 
-
-
 module.exports = app;
 
 // IMPORTANT-NOTE: IN JSON EVERYTHING HAS TO BE IN QUOTE strictly DOUBLE QUOTE
 
-
-
 // npm i express-rate-limit
-// this is to install package for implementing rate limit: which is a security measure to prevent to many request from a single one IP to the server in a certain amount of time 
+// this is to install package for implementing rate limit: which is a security measure to prevent to many request from a single one IP to the server in a certain amount of time
 
-
-// npm i helmet 
+// npm i helmet
 // this helmet package is used for setting security HTTP headers
 
-
-
 //npm i express-mongo-sanitize
-// this package is used to sanitize or prevent query injection attack like logging email: {$gte: ''} which could access data base as true from invader 
+// this package is used to sanitize or prevent query injection attack like logging email: {$gte: ''} which could access data base as true from invader
 
-
-
-// npm i xss-clean 
-// this is also for sanitization 
-
+// npm i xss-clean
+// this is also for sanitization
 
 // npm i hpp
-// this prevent parameter pollution 
-
+// this prevent parameter pollution
 
 // npm i compression
 // this will compress our json response from the server
 
-
-// npm i cors 
+// npm i cors
 // CORS- CROSS ORIGIN RESOURCE SHARING, this make our API to be accessible by external user and not only there server , it simply a middleware
--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,5 +1,6 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const Tour = require('./../models/tourModel');
+const User = require('./../models/userModel');
 const Booking = require('./../models/bookingModel');
 const catchAsync = require('./../utils/catchAsync');
 const handlerFactory = require('./../controllers/handlerFactory');
@@ -12,9 +13,10 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   //for this install the stripe npm package: npm i stripe
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'], // card is for credit-card
-    success_url: `${req.protocol}://${req.get('host')}/?tour=${
-      req.params.tourID
-    }&user=${req.user.id}&price=${tour.price}`, //  immediately the card has been successfully charged and purchase successful, user will be redirected to this url, it will be a get request
+    // success_url: `${req.protocol}://${req.get('host')}/?tour=${
+    //   req.params.tourID
+    // }&user=${req.user.id}&price=${tour.price}`, //  immediately the card has been successfully charged and purchase successful, user will be redirected to this url, it will be a get request
+    success_url: `${req.protocol}://${req.get('host')}/my-tours`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`, // page showed if the user choose to cancel the payment
     customer_email: req.user.email, // because the bookings route is protected the user details will be on the req
     client_reference_id: req.params.tourID,
@@ -25,7 +27,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
           currency: 'usd',
           product_data: {
             name: `${tour.name} Tour`,
-            images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+            images: [`${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`],
             description: tour.summary,
           },
           unit_amount: tour.price * 100,
@@ -47,22 +49,57 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   });
 });
 
-// to create a bookings we need the user's id the tours ID and the price
+// to create a bookings we need the user's id the tours ID and the price, this is unsafe practice, safe practice involves using WEBHOOKS
 
-exports.createBookingCheckout = catchAsync(async (req, res, next) => {
-  // This is only TEMPORARY, because its UNSECURE: everyone can make bookings without paying 
-  const { tour, user, price } = req.query;
+// exports.createBookingCheckout = catchAsync(async (req, res, next) => {
+//   // This is only TEMPORARY, because its UNSECURE: everyone can make bookings without paying
+//   const { tour, user, price } = req.query;
 
-  if (!tour && !user && !price) return next();
+//   if (!tour && !user && !price) return next();
 
+//   await Booking.create({ tour, user, price });
+
+//   res.redirect(req.originalUrl.split('?')[0])
+// });
+
+const createBookingCheckout = async (sessionData) => {
+  const tour = sessionData.client_reference_id;
+  const user = (await User.findOne({ email: sessionData.customer_email })).id; // read the ID from the queried doc
+  const price = sessionData.line_items[0].price_data.unit_amount / 100;
   await Booking.create({ tour, user, price });
+};
 
-  res.redirect(req.originalUrl.split('?')[0])
-});
+// PAYMENT WITH STRIPE WEBHOOK
+// stripe will call this function when the payment is successful
+// the function receives the body for the request with the signature and webhook secret to create an event which will contain a session, using that session data we can create new booking in the database
+exports.webhookCheckout = (req, res, next) => {
+  const signature = req.headers['stripe-signature'];
 
+  let event;
+  try {
+    event = stripe.webhooks.constructEvent(
+      req.body,
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET
+    ); // the body here is available in raw form i.e as a stream
+  } catch (err) {
+    return res.status(400).send(`webhook error: ${err.message}
+    `);
+
+    // we receive the response here cos it is stripe that will call the webhook
+  }
+
+  if (event.type === 'checkout.session.completed') {
+    createBookingCheckout(event.data.object);
+
+    res.status(200).json({ received: true }); // response to stripe
+  }
+};
 
 exports.createBooking = handlerFactory.createOne(Booking);
-exports.getBooking = handlerFactory.getOne(Booking)
-exports.getAllBookings = handlerFactory.getAll(Booking)
-exports.updateBooking = handlerFactory.updateOne(Booking)
-exports.deleteBooking = handlerFactory.deleteOne(Booking)
+exports.getBooking = handlerFactory.getOne(Booking);
+exports.getAllBookings = handlerFactory.getAll(Booking);
+exports.updateBooking = handlerFactory.updateOne(Booking);
+exports.deleteBooking = handlerFactory.deleteOne(Booking);
+
+// WE USE STRIPE WEBHOOKS WHEN THE APP HAS ALREADY BEEN DEPLOYED
