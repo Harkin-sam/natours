dbde0d2745ca4d79b1d723432470f32bcb686c37
Samuel Akinfolarin <Akinfolarin001@gmail.com>
2023-07-28 13:21:46 +0100

    Finalizing

--- public/js/index.js
+++ public/js/index.js
@@ -92,7 +92,7 @@ if (bookBtn) {
 
 const alertMessage = document.querySelector('body').dataset.alert
 if (alertMessage){
-  showAlert("success", alertMessage, 20)
+  showAlert("success", alertMessage, 10)
 }
 
 
